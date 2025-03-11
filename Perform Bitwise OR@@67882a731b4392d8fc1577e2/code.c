f2ba920969dcab02835fab2bdb3d98b46709c990
Yashti Garg <yashtigarg02@gmail.com>
2025-03-11 16:20:57 +0530

    Solution to Perform Bitwise OR

--- Perform Bitwise OR@@67882a731b4392d8fc1577e2/code.c
+++ Perform Bitwise OR@@67882a731b4392d8fc1577e2/code.c
@@ -1,21 +1,6 @@
-// Your code here...
-#include<stdio.h>
+#include <stdio.h>
 int main(){
-    int a,flag=1;
-    scanf("%d",&a);
-if(a<=1){
-    printf("Not Prime");
-}
-for(int i=2;i*i<=a;i++){
-    if(a%i==0){
-        flag=0;
-        break;
-    }
-}
-if(flag){
-    printf("Prime");
-}
-else{
-    printf("Not Prime");
-}
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("%d",a|b);
 }
