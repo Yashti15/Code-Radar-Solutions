51971c98d3667f95cce4af4c4441598ae4b32777
Yashti Garg <yashtigarg02@gmail.com>
2025-03-28 19:53:56 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -21,7 +21,15 @@ int main(){
             }
         }
     }
-    printf("%d %d",pair1,pair2);
+    if(pair1>pair2){
+        printf("%d %d",pair1,pair2);
+    }
+    else if(pair2>pair1){
+        printf("%d %d",pair2,pair1);
+    }
+    else{
+        printf("-1");
+    }
 
 
 
